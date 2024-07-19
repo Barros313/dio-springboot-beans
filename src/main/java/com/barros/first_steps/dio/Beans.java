c43a48d68138f5b1602cc794fb99c6c665f6fcc2
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-19 11:03:24 -0300

    Added @Configuration tag to Beans.java to be compatible with other spring versions
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- src/main/java/com/barros/first_steps/dio/Beans.java
+++ src/main/java/com/barros/first_steps/dio/Beans.java
@@ -2,7 +2,9 @@ package com.barros.first_steps.dio;
 
 import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class Beans {
     @Bean
     public Gson gson() {
